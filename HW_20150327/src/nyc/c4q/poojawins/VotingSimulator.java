7981498f9744d354b4c72197249aaa4e6a77ea70
Pooja Pasawala <pooja@basicer.com>
2015-03-28 00:37:16 -0400

    Add completed VotingSimulator.

--- HW_20150327/src/nyc/c4q/poojawins/VotingSimulator.java
+++ HW_20150327/src/nyc/c4q/poojawins/VotingSimulator.java
@@ -4,11 +4,26 @@ public class VotingSimulator {
 
     public static void main(String[] args) {
         //1. Create an Election object, and given the Election a name
-        //2. Create a few Contender objects. Add these to the Election object. Make sure that the contender names are distinct!
-        //3. Create a ElectionManager object. Ask it to manage the Election object created above.
-        //4. Ask the ElectionManager to initiatePolling
-        //5. Follow the instructions on the console. After each round of polling you will be asked(within the console) whether you want to continue or not.
-        //6. Ask the ElectionManager to displayResults
+        Election myElection = new Election("OMG ELECTION");
 
+        //2. Create a few Contender objects. Add these to the Election object. Make sure that the contender names are distinct!
+        Contender pooja = new Contender("Pooja");
+        Contender rob = new Contender("Rob");
+        Contender other = new Contender("Other");
+        myElection.add(pooja);
+        myElection.add(rob);
+        myElection.add(other);
+
+        //3. Create a ElectionManager object. Ask it to manage the Election object created above.
+        ElectionManager myElectionManager = new ElectionManager();
+        myElectionManager.manage(myElection);
+
+        //4. Ask the ElectionManager to initiatePolling
+        myElectionManager.initiatePolling();
+
+        //5. Follow the instructions on the console. After each round of polling you will be asked(within the console) whether you want to continue or not.
+
+        //6. Ask the ElectionManager to displayResults
+        myElectionManager.displayResults();
     }
 }
