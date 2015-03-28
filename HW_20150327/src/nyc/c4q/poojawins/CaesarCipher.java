aedb5fca75322833c3074e4329c3597f12dbfda8
Pooja Pasawala <pooja@basicer.com>
2015-03-28 00:29:37 -0400

    Refactor codeBreaker (don't need two loops).

--- HW_20150327/src/nyc/c4q/poojawins/CaesarCipher.java
+++ HW_20150327/src/nyc/c4q/poojawins/CaesarCipher.java
@@ -29,16 +29,12 @@ public class CaesarCipher {
     }
 
     public static boolean codeBreaker(String one, String two) {
-        for(int i = 1; i < 27; i++) {
-            String first = encode(one, i);
-            for (int j = 1; j < 27; j++) {
-                String second = encode(two, j);
-                if (first.equals(second)) {
-                    return true;
-                }
+        for (int i = 1; i < 27; i++) {
+            if (one.equals(encode(two, i))) {
+                return true;
             }
-            return false;
         }
+        return false;
     }
 
     public static void main(String[] args){
